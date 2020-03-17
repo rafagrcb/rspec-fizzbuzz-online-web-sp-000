9bbe6a7ca6f820d2be028a3db4e1d2200c5c169f
Rafael Garcia <rafagrcb@gmail.com>
2020-03-17 19:37:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+    elsif int % 5 == 0
     "Buzz"
-  elsif int % 15 == 0
+    elsif int % 15 == 0
     "FizzBuzz"
-  else int % 4 == 0
-    nil
-  end
+  else int % 
 end
